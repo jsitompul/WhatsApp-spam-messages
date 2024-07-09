0cbb6885405cc27f91c6a8ebbf1e3ecd04bd7168
jsitompul <151981311+jsitompul@users.noreply.github.com>
2024-07-09 13:45:24 +0700

    Update wa-spam.py

--- wa-spam.py
+++ wa-spam.py
@@ -16,7 +16,7 @@ message = ['Cieee penipu', 'Dasar tukang tipu', 'Ketauan nihh yee', 'Lu kurang j
 time.sleep(5)
 
 # Loop untuk mengirimkan pesan sebanyak 1000 kali
-for i in range(1000):
+for i in range(20):
     # Memilih pesan secara acak dari daftar pesan
     ejekan = random.choice(message)
     # Menulis pesan terpilih ke dalam jendela chat
