dfbfc0565c9441dbf3af079b83686c4957695dbd
jsitompul <151981311+jsitompul@users.noreply.github.com>
2024-07-09 09:16:42 +0700

    Update wa-spam.py

--- wa-spam.py
+++ wa-spam.py
@@ -1,14 +1,24 @@
+# Author: Jimmy Sitompul
+
+# Import modul random untuk memilih pesan secara acak
 import random
+# Import modul pyautogui dengan alias pg untuk melakukan otomatisasi GUI
 import pyautogui as pg
+# Import modul time untuk mengatur waktu tunda
 import time
 
+# Daftar pesan yang akan dikirimkan secara acak
+message = ['Cieee penipu', 'Dasar tukang tipu', 'Ketauan nihh yee', 'Lu kurang jago bro hahaha', 'SDM RENDAH']
 
-message = ['Cieee penipu','Dasar tukang tipu','Ketauan nihh yee','Lu kurang jago bro hahaha','SDM RENDAH']
-
+# Menunggu selama 5 detik sebelum memulai pengiriman pesan
 time.sleep(5)
 
+# Loop untuk mengirimkan pesan sebanyak 1000 kali
 for i in range(1000):
+    # Memilih pesan secara acak dari daftar pesan
     ejekan = random.choice(message)
+    # Menulis pesan terpilih ke dalam jendela chat
     pg.write(ejekan)
+    # Menekan tombol enter untuk mengirimkan pesan
     pg.press('enter')
 
